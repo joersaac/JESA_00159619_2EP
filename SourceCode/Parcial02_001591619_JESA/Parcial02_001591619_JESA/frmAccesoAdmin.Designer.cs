733ade75b114126001d28838468c5478f7328b4c
unknown <00159619@uca.edu.sv>
2020-05-27 13:20:55 -0600

    Add: uscCrearUsuario.cs

--- SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/frmAccesoAdmin.Designer.cs
+++ SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/frmAccesoAdmin.Designer.cs
@@ -28,12 +28,188 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAccesoAdmin));
+            this.tabAdmin = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.tabPage5 = new System.Windows.Forms.TabPage();
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.tloMantenimientoU = new System.Windows.Forms.TableLayoutPanel();
+            this.dgvUsuario = new System.Windows.Forms.DataGridView();
+            this.btnCrearU = new System.Windows.Forms.Button();
+            this.btnEliminarU = new System.Windows.Forms.Button();
+            this.tabAdmin.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
+            this.tloMantenimientoU.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabAdmin
+            // 
+            this.tabAdmin.Controls.Add(this.tabPage1);
+            this.tabAdmin.Controls.Add(this.tabPage2);
+            this.tabAdmin.Controls.Add(this.tabPage3);
+            this.tabAdmin.Controls.Add(this.tabPage4);
+            this.tabAdmin.Controls.Add(this.tabPage5);
+            this.tabAdmin.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabAdmin.Location = new System.Drawing.Point(0, 0);
+            this.tabAdmin.Name = "tabAdmin";
+            this.tabAdmin.SelectedIndex = 0;
+            this.tabAdmin.Size = new System.Drawing.Size(857, 500);
+            this.tabAdmin.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.tabPage1.Controls.Add(this.picLogo);
+            this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(849, 467);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "General";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.tabPage2.Controls.Add(this.tloMantenimientoU);
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(849, 467);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Mantenimiento usuario";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.tabPage3.Location = new System.Drawing.Point(4, 29);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(849, 467);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Mantenimiento negocios";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.tabPage4.Location = new System.Drawing.Point(4, 29);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(849, 467);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Mantenimiento productos";
+            // 
+            // tabPage5
+            // 
+            this.tabPage5.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.tabPage5.Location = new System.Drawing.Point(4, 29);
+            this.tabPage5.Name = "tabPage5";
+            this.tabPage5.Size = new System.Drawing.Size(849, 467);
+            this.tabPage5.TabIndex = 4;
+            this.tabPage5.Text = "Historial ordenes";
+            // 
+            // picLogo
+            // 
+            this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
+            this.picLogo.InitialImage = ((System.Drawing.Image)(resources.GetObject("picLogo.InitialImage")));
+            this.picLogo.Location = new System.Drawing.Point(292, 78);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(268, 256);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLogo.TabIndex = 1;
+            this.picLogo.TabStop = false;
+            // 
+            // tloMantenimientoU
+            // 
+            this.tloMantenimientoU.ColumnCount = 3;
+            this.tloMantenimientoU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tloMantenimientoU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tloMantenimientoU.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tloMantenimientoU.Controls.Add(this.dgvUsuario, 0, 0);
+            this.tloMantenimientoU.Controls.Add(this.btnCrearU, 1, 1);
+            this.tloMantenimientoU.Controls.Add(this.btnEliminarU, 2, 1);
+            this.tloMantenimientoU.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tloMantenimientoU.Location = new System.Drawing.Point(3, 3);
+            this.tloMantenimientoU.Name = "tloMantenimientoU";
+            this.tloMantenimientoU.RowCount = 2;
+            this.tloMantenimientoU.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tloMantenimientoU.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tloMantenimientoU.Size = new System.Drawing.Size(843, 461);
+            this.tloMantenimientoU.TabIndex = 0;
+            // 
+            // dgvUsuario
+            // 
+            this.dgvUsuario.BackgroundColor = System.Drawing.Color.White;
+            this.dgvUsuario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvUsuario.Location = new System.Drawing.Point(3, 3);
+            this.dgvUsuario.Name = "dgvUsuario";
+            this.dgvUsuario.RowHeadersWidth = 62;
+            this.tloMantenimientoU.SetRowSpan(this.dgvUsuario, 2);
+            this.dgvUsuario.RowTemplate.Height = 28;
+            this.dgvUsuario.Size = new System.Drawing.Size(415, 455);
+            this.dgvUsuario.TabIndex = 0;
+            // 
+            // btnCrearU
+            // 
+            this.btnCrearU.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnCrearU.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCrearU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrearU.ForeColor = System.Drawing.Color.White;
+            this.btnCrearU.Location = new System.Drawing.Point(424, 417);
+            this.btnCrearU.Name = "btnCrearU";
+            this.btnCrearU.Size = new System.Drawing.Size(204, 41);
+            this.btnCrearU.TabIndex = 6;
+            this.btnCrearU.Text = "Crear";
+            this.btnCrearU.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminarU
+            // 
+            this.btnEliminarU.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnEliminarU.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEliminarU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarU.ForeColor = System.Drawing.Color.White;
+            this.btnEliminarU.Location = new System.Drawing.Point(634, 417);
+            this.btnEliminarU.Name = "btnEliminarU";
+            this.btnEliminarU.Size = new System.Drawing.Size(206, 41);
+            this.btnEliminarU.TabIndex = 7;
+            this.btnEliminarU.Text = "Eliminar";
+            this.btnEliminarU.UseVisualStyleBackColor = false;
+            // 
+            // frmAccesoAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(857, 500);
+            this.Controls.Add(this.tabAdmin);
+            this.Name = "frmAccesoAdmin";
             this.Text = "frmAccesoAdmin";
+            this.tabAdmin.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
+            this.tloMantenimientoU.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuario)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabAdmin;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage5;
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.TableLayoutPanel tloMantenimientoU;
+        private System.Windows.Forms.DataGridView dgvUsuario;
+        private System.Windows.Forms.Button btnCrearU;
+        private System.Windows.Forms.Button btnEliminarU;
     }
 }
---
+++ SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/uscCrearUsuario.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Parcial02_001591619_JESA
+{
+    partial class uscCrearUsuario
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tlpCrearUsuario = new System.Windows.Forms.TableLayoutPanel();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.btnCrearU = new System.Windows.Forms.Button();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblTipoAcces = new System.Windows.Forms.Label();
+            this.txbFullName = new System.Windows.Forms.TextBox();
+            this.txbUserName = new System.Windows.Forms.TextBox();
+            this.txbTipoAcces = new System.Windows.Forms.TextBox();
+            this.tlpCrearUsuario.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tlpCrearUsuario
+            // 
+            this.tlpCrearUsuario.ColumnCount = 3;
+            this.tlpCrearUsuario.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tlpCrearUsuario.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCrearUsuario.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tlpCrearUsuario.Controls.Add(this.lblFullName, 0, 1);
+            this.tlpCrearUsuario.Controls.Add(this.btnCrearU, 0, 7);
+            this.tlpCrearUsuario.Controls.Add(this.lblUserName, 0, 3);
+            this.tlpCrearUsuario.Controls.Add(this.lblTipoAcces, 0, 5);
+            this.tlpCrearUsuario.Controls.Add(this.txbFullName, 1, 1);
+            this.tlpCrearUsuario.Controls.Add(this.txbUserName, 1, 3);
+            this.tlpCrearUsuario.Controls.Add(this.txbTipoAcces, 1, 5);
+            this.tlpCrearUsuario.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tlpCrearUsuario.Location = new System.Drawing.Point(0, 0);
+            this.tlpCrearUsuario.Name = "tlpCrearUsuario";
+            this.tlpCrearUsuario.RowCount = 9;
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 11.11111F));
+            this.tlpCrearUsuario.Size = new System.Drawing.Size(688, 462);
+            this.tlpCrearUsuario.TabIndex = 0;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFullName.ForeColor = System.Drawing.Color.White;
+            this.lblFullName.Location = new System.Drawing.Point(63, 62);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(218, 29);
+            this.lblFullName.TabIndex = 2;
+            this.lblFullName.Text = "Nombre Completo:";
+            this.lblFullName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnCrearU
+            // 
+            this.btnCrearU.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCrearU.BackColor = System.Drawing.Color.SlateBlue;
+            this.tlpCrearUsuario.SetColumnSpan(this.btnCrearU, 3);
+            this.btnCrearU.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCrearU.ForeColor = System.Drawing.Color.White;
+            this.btnCrearU.Location = new System.Drawing.Point(242, 362);
+            this.btnCrearU.Name = "btnCrearU";
+            this.btnCrearU.Size = new System.Drawing.Size(204, 41);
+            this.btnCrearU.TabIndex = 7;
+            this.btnCrearU.Text = "Crear";
+            this.btnCrearU.UseVisualStyleBackColor = false;
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.White;
+            this.lblUserName.Location = new System.Drawing.Point(59, 164);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(226, 29);
+            this.lblUserName.TabIndex = 8;
+            this.lblUserName.Text = "Nombre de usuario:";
+            this.lblUserName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTipoAcces
+            // 
+            this.lblTipoAcces.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lblTipoAcces.AutoSize = true;
+            this.lblTipoAcces.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoAcces.ForeColor = System.Drawing.Color.White;
+            this.lblTipoAcces.Location = new System.Drawing.Point(79, 266);
+            this.lblTipoAcces.Name = "lblTipoAcces";
+            this.lblTipoAcces.Size = new System.Drawing.Size(186, 29);
+            this.lblTipoAcces.TabIndex = 9;
+            this.lblTipoAcces.Text = "Tipo de acceso:";
+            this.lblTipoAcces.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txbFullName
+            // 
+            this.txbFullName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tlpCrearUsuario.SetColumnSpan(this.txbFullName, 2);
+            this.txbFullName.Location = new System.Drawing.Point(402, 63);
+            this.txbFullName.Name = "txbFullName";
+            this.txbFullName.Size = new System.Drawing.Size(227, 26);
+            this.txbFullName.TabIndex = 10;
+            // 
+            // txbUserName
+            // 
+            this.txbUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tlpCrearUsuario.SetColumnSpan(this.txbUserName, 2);
+            this.txbUserName.Location = new System.Drawing.Point(402, 165);
+            this.txbUserName.Name = "txbUserName";
+            this.txbUserName.Size = new System.Drawing.Size(227, 26);
+            this.txbUserName.TabIndex = 11;
+            // 
+            // txbTipoAcces
+            // 
+            this.txbTipoAcces.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.tlpCrearUsuario.SetColumnSpan(this.txbTipoAcces, 2);
+            this.txbTipoAcces.Location = new System.Drawing.Point(402, 267);
+            this.txbTipoAcces.Name = "txbTipoAcces";
+            this.txbTipoAcces.Size = new System.Drawing.Size(227, 26);
+            this.txbTipoAcces.TabIndex = 12;
+            // 
+            // uscCrearUsuario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.Controls.Add(this.tlpCrearUsuario);
+            this.Name = "uscCrearUsuario";
+            this.Size = new System.Drawing.Size(688, 462);
+            this.tlpCrearUsuario.ResumeLayout(false);
+            this.tlpCrearUsuario.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tlpCrearUsuario;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.Button btnCrearU;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblTipoAcces;
+        private System.Windows.Forms.TextBox txbFullName;
+        private System.Windows.Forms.TextBox txbUserName;
+        private System.Windows.Forms.TextBox txbTipoAcces;
+    }
+}
