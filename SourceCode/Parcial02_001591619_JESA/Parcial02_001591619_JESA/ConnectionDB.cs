89f17440ac4243fa5c8a39c8020e659b68e22195
unknown <00159619@uca.edu.sv>
2020-05-27 11:08:23 -0600

    Add: ConnectionDB.cs

--- SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/ConnectionDB.cs
+++ SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/ConnectionDB.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data;
+﻿using System.Data;
 using Npgsql;
 
 namespace Parcial02_001591619_JESA
