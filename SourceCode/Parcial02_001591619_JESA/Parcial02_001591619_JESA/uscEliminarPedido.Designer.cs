a360c38e1798a0a538a6d22c6d9546735638f247
unknown <00159619@uca.edu.sv>
2020-05-28 00:17:40 -0600

    Add: capturas de pantalla

--- SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/uscEliminarPedido.Designer.cs
+++ SourceCode/Parcial02_001591619_JESA/Parcial02_001591619_JESA/uscEliminarPedido.Designer.cs
@@ -29,9 +29,9 @@
         private void InitializeComponent()
         {
             this.tlpEliminarPedido = new System.Windows.Forms.TableLayoutPanel();
-            this.lblOrden = new System.Windows.Forms.Label();
+            this.btnEliminarOrden = new System.Windows.Forms.Button();
             this.cmbOrdenes = new System.Windows.Forms.ComboBox();
-            this.btnRealizarOrden = new System.Windows.Forms.Button();
+            this.lblOrden = new System.Windows.Forms.Label();
             this.tlpEliminarPedido.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -40,7 +40,7 @@
             this.tlpEliminarPedido.ColumnCount = 2;
             this.tlpEliminarPedido.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tlpEliminarPedido.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tlpEliminarPedido.Controls.Add(this.btnRealizarOrden, 0, 1);
+            this.tlpEliminarPedido.Controls.Add(this.btnEliminarOrden, 0, 1);
             this.tlpEliminarPedido.Controls.Add(this.cmbOrdenes, 1, 0);
             this.tlpEliminarPedido.Controls.Add(this.lblOrden, 0, 0);
             this.tlpEliminarPedido.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -51,6 +51,31 @@
             this.tlpEliminarPedido.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tlpEliminarPedido.Size = new System.Drawing.Size(568, 467);
             this.tlpEliminarPedido.TabIndex = 0;
+            // 
+            // btnEliminarOrden
+            // 
+            this.btnEliminarOrden.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnEliminarOrden.BackColor = System.Drawing.Color.SlateBlue;
+            this.tlpEliminarPedido.SetColumnSpan(this.btnEliminarOrden, 2);
+            this.btnEliminarOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEliminarOrden.ForeColor = System.Drawing.Color.White;
+            this.btnEliminarOrden.Location = new System.Drawing.Point(138, 320);
+            this.btnEliminarOrden.Name = "btnEliminarOrden";
+            this.btnEliminarOrden.Size = new System.Drawing.Size(292, 60);
+            this.btnEliminarOrden.TabIndex = 19;
+            this.btnEliminarOrden.Text = "Eliminar orden";
+            this.btnEliminarOrden.UseVisualStyleBackColor = false;
+            this.btnEliminarOrden.Click += new System.EventHandler(this.btnRealizarOrden_Click);
+            // 
+            // cmbOrdenes
+            // 
+            this.cmbOrdenes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbOrdenes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOrdenes.FormattingEnabled = true;
+            this.cmbOrdenes.Location = new System.Drawing.Point(287, 102);
+            this.cmbOrdenes.Name = "cmbOrdenes";
+            this.cmbOrdenes.Size = new System.Drawing.Size(278, 28);
+            this.cmbOrdenes.TabIndex = 18;
             // 
             // lblOrden
             // 
@@ -64,31 +89,6 @@
             this.lblOrden.TabIndex = 17;
             this.lblOrden.Text = "Ordenes:";
             this.lblOrden.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // cmbOrdenes
-            // 
-            this.cmbOrdenes.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cmbOrdenes.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.cmbOrdenes.FormattingEnabled = true;
-            this.cmbOrdenes.Location = new System.Drawing.Point(287, 102);
-            this.cmbOrdenes.Name = "cmbOrdenes";
-            this.cmbOrdenes.Size = new System.Drawing.Size(278, 28);
-            this.cmbOrdenes.TabIndex = 18;
-            // 
-            // btnRealizarOrden
-            // 
-            this.btnRealizarOrden.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnRealizarOrden.BackColor = System.Drawing.Color.SlateBlue;
-            this.tlpEliminarPedido.SetColumnSpan(this.btnRealizarOrden, 2);
-            this.btnRealizarOrden.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnRealizarOrden.ForeColor = System.Drawing.Color.White;
-            this.btnRealizarOrden.Location = new System.Drawing.Point(138, 320);
-            this.btnRealizarOrden.Name = "btnRealizarOrden";
-            this.btnRealizarOrden.Size = new System.Drawing.Size(292, 60);
-            this.btnRealizarOrden.TabIndex = 19;
-            this.btnRealizarOrden.Text = "Realizar orden";
-            this.btnRealizarOrden.UseVisualStyleBackColor = false;
-            this.btnRealizarOrden.Click += new System.EventHandler(this.btnRealizarOrden_Click);
             // 
             // uscEliminarPedido
             // 
@@ -110,6 +110,6 @@
         private System.Windows.Forms.TableLayoutPanel tlpEliminarPedido;
         private System.Windows.Forms.Label lblOrden;
         private System.Windows.Forms.ComboBox cmbOrdenes;
-        private System.Windows.Forms.Button btnRealizarOrden;
+        private System.Windows.Forms.Button btnEliminarOrden;
     }
 }
